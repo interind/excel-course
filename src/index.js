73dd0d13dbb1715504b087406fa127309f44f42c
Aleksandr <interind@yandex.ru>
2021-01-28 10:04:06 +0300

    Redux createStore

--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ import { Table } from './components/table/Table';
 import { Header } from './components/header/Header';
 import { Formula } from './components/formula/Formula';
 import { Toolbar } from './components/toolbar/Toolbar';
+import { createStore } from './core/createStore';
+import { rootReducer } from './redux/rootReducer';
 
+const store = createStore(rootReducer);
 
 const excel = new Excel('#app', {
   components: [Header, Toolbar, Formula, Table],
+  store,
 });
 
 excel.render();
